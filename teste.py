a200c24e9f912b0992e7d830cdae59c15437ca9f
Henrique Poyatos <poyatos@fiap.com.br>
2021-04-14 11:24:46 -0300

    Mais uma frase para o novo sistema

--- teste.py
+++ teste.py
@@ -2,4 +2,7 @@ nome="Poyatos"
 print("Alô mundo, {}!\n".format(nome))
 
 #Nova funcionalidade
-print("Isso aqui é novo!")
+print("Isso aqui é novo!\n")
+
+#Nova funcionalidade 2
+print("nova frase!")
